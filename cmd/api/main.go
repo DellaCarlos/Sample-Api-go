bd6395bb84a75e2ac97493d6e2fad6a523ebf6ad
Carlos <carlos.cdp3.14@gmail.com>
2026-04-03 21:06:48 -0300

    feat: middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,27 +6,33 @@ import (
 	"sample-api-go/internal/repositories"
 	usecase "sample-api-go/internal/usecase"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
+	// middleware
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173"}, // porta vite
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
+
 	dbConnection, err := database.ConnectDB()
 	if err != nil {
 		panic(err)
 	}
 
-	SampleRepository := repositories.NewSampleRepository(dbConnection)  // camada repo
-	SampletUseCase := usecase.NewCreateSampleUseCase(SampleRepository)  // camada use-case
-	SampleController := controllers.NewSampleController(SampletUseCase) // camada controller
+	SampleRepository := repositories.NewSampleRepository(dbConnection)
+	SampletUseCase := usecase.NewCreateSampleUseCase(SampleRepository)
+	SampleController := controllers.NewSampleController(SampletUseCase)
 
-	// endpoinst.
 	server.GET("/samples", SampleController.GetSamples)
 	server.GET("/samples/:id_sample", SampleController.GetSampleByID)
 	server.POST("/sample", SampleController.CreateSample)
 	server.DELETE("/sampledelete/:id_sample", SampleController.SoftDeleteSampleByID)
 
-	// Start server on port 8080 (default)
 	server.Run()
 }
